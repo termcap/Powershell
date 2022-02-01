f3ebcdac58d8c45857d372585ef711b2d780498e
termcap <27614065+termcap@users.noreply.github.com>
2022-02-01 19:52:12 +0300

    Update does-cert-exist.ps1

--- does-cert-exist.ps1
+++ does-cert-exist.ps1
@@ -1,1 +1,7 @@
-Get-ChildItem Cert:\LocalMachine\Root\*  | Where-Object Thumbprint -eq "3679CA35668772304D30A5FB873B0FA77BB70D54"
+$content=Get-ChildItem Cert:\LocalMachine\Root\*  | Where-Object Thumbprint -eq "3679CA35668772304D30A5FB873B0FA77BB70D54"
+ 
+ if($content) {
+	 Write-Host "Exists"
+ } else {
+	Write-Host "Does not exist"
+ }
